2377b3f414df89a045543e896e7872529a24b248
Kyle Espinola <ksespinola@gmail.com>
2017-06-05 14:05:34 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { mergeDeepLeft } = require('ramda');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = ({ config }) => {
+  config
+    .when(process.env.NODE_ENV === 'production', config => {
+      config.output.filename('[name].bundle.js')
+    });
+
+  config.plugins.delete('html').end();
+
+  config.module
+    .rule('compile')
+    .use('babel')
+    .tap(mergeDeepLeft({ presets: ['babel-preset-react', 'babel-preset-stage-0'] }));
+
+  config.module.rules.delete('style');
+
+  const splitCss = config.module
+    .rule('style')
+    .test(/\.css$/);
+
+  const extract = ExtractTextPlugin.extract({ use: 'css-loader?modules&localIdentName=[path][name]__[local]--[hash:base64:5]', fallback: 'style-loader', publicPath: '/build' });
+
+  extract.forEach(({ loader, options }) => splitCss.use(loader).loader(loader).options(options));
+
+config
+  .plugin('style')
+  .use(ExtractTextPlugin, [{
+    filename: '[name].bundle.css',
+    allChunks: true
+  }]);
+}
