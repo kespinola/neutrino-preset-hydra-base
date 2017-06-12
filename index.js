29c8d65f81f5c8d0aba214bdd216eeccbb2952d4
Kyle Espinola <ksespinola@gmail.com>
2017-06-12 14:09:12 -0700

    remove chunk plugin from prod

--- index.js
+++ index.js
@@ -4,30 +4,31 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = ({ config }) => {
   config
     .when(process.env.NODE_ENV === 'production', config => {
+      config.plugin.delete('chunk').end()
       config.output.filename('[name].bundle.js')
     });
 
-  config.plugins.delete('html').end();
+  config.plugins.delete('html').end()
 
   config.module
     .rule('compile')
     .use('babel')
-    .tap(mergeDeepLeft({ presets: ['babel-preset-react', 'babel-preset-stage-0'] }));
+    .tap(mergeDeepLeft({ presets: ['babel-preset-react', 'babel-preset-stage-0'] }))
 
-  config.module.rules.delete('style');
+  config.module.rules.delete('style')
 
   const splitCss = config.module
     .rule('style')
-    .test(/\.css$/);
+    .test(/\.css$/)
 
-  const extract = ExtractTextPlugin.extract({ use: ['css-loader?modules&localIdentName=[local]__[path][name]__[hash:base64:5]&importLoaders=1', 'sass-loader'], fallback: 'style-loader', publicPath: '/build' });
+  const extract = ExtractTextPlugin.extract({ use: ['css-loader?modules&localIdentName=[local]__[path][name]__[hash:base64:5]&importLoaders=1', 'sass-loader'], fallback: 'style-loader', publicPath: '/build' })
 
-  extract.forEach(({ loader, options }) => splitCss.use(loader).loader(loader).options(options));
+  extract.forEach(({ loader, options }) => splitCss.use(loader).loader(loader).options(options))
 
 config
   .plugin('style')
   .use(ExtractTextPlugin, [{
     filename: '[name].bundle.css',
     allChunks: true
-  }]);
+  }])
 }
